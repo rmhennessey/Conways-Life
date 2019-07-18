9af4331b34d641912b1a109813d8d08ea21d8916
RMH <rory.hennessey@gmail.com>
2019-07-18 09:25:35 -0400

    game complete using guide.

---
+++ conway-game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main.js'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to the Jungle</h1>
+     <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ conway-game/src/components/Grid.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Box from './Box.js'
+
+export default class Grid extends Component {
+    render() {
+        let width = this.props.cols * 25;
+        let rowsArr = [];
+
+        let boxClass = "";
+
+        for (let i = 0; i < this.props.rows; i++) {
+            for (let j = 0; j < this.props.cols; j++) {
+                let boxId = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArr.push(
+                    <Box 
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId={boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                );
+            }
+        }
+
+        return (
+            <div className="grid" style={{width: width}}>
+                {rowsArr}
+            </div>
+        )
+    }
+}
