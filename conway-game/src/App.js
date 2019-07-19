0987e0b84bce877937f7ca24b7be02f9d9c9f05b
Rory Hennessey <rory.hennessey@gmail.com>
2019-07-19 11:39:25 -0400

    rules added

--- conway-game/src/App.js
+++ conway-game/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main.js'
+import Rules from './components/Rules'
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to the Jungle</h1>
      <Main />
+     <Rules />
     </div>
   );
 }
